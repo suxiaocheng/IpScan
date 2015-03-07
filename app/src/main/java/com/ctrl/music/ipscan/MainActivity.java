c4cbefc0189b9033e1d72cff2ef2db3300e882a6
suxiaocheng <suxiaocheng2010@gmail.com>
2015-03-08 00:13:50 +0800

    Ip scan function working ok

--- app/src/main/java/com/ctrl/music/ipscan/MainActivity.java
+++ app/src/main/java/com/ctrl/music/ipscan/MainActivity.java
@@ -1,10 +1,19 @@
 package com.ctrl.music.ipscan;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,23 +22,93 @@ public class MainActivity extends ActionBarActivity {
     private NetTool NetTool = new NetTool();
     private TextView TWCurrentIP;
     private String CSCurrentIP;
+    private Button BTScan;
+    private int BTScanCount = 0x0;
+
+    private ArrayAdapter<String> APIPList;
+    private ListView LVIPList;
+
+    private Handler handler = new Handler();
+    private ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TWCurrentIP = (TextView)findViewById(R.id.TextViewCurrentIP);
+        TWCurrentIP = (TextView) findViewById(R.id.TextViewCurrentIP);
         CSCurrentIP = NetTool.getLocAddress();
-        if(TWCurrentIP != null){
-            if(CSCurrentIP != null) {
+        if (TWCurrentIP != null) {
+            if (CSCurrentIP != null) {
                 TWCurrentIP.setText(CSCurrentIP);
-            }else{
+            } else {
                 TWCurrentIP.setText("Can't Get IP address");
             }
         }
+
+        BTScan = (Button) findViewById(R.id.buttonScanIP);
+
+        // construct the list view adapter
+        APIPList = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1);
+
+        LVIPList = (ListView) findViewById(R.id.listViewActivityIP);
+        LVIPList.setAdapter(APIPList);
     }
 
+    private void ScanThread(final String ip) {
+        executorService.submit(new Runnable() {
+            public void run() {
+                boolean status;
+                status = NetTool.ping(ip);
+                if (status == true) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            APIPList.add(ip);
+                            LVIPList.setAdapter(APIPList);
+                        }
+                    });
+                }
+                BTScanCount++;
+                Log.d(TAG, "BTScanCount:"+BTScanCount);
+                if (BTScanCount == 256) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            BTScan.setEnabled(true);
+                        }
+                    });
+                }
+            }
+
+        });
+    }
+
+    public void ScanClick(View v) {
+        int count;
+        String IPTmp;
+        int csEnd;
+
+        APIPList.clear();
+        if (CSCurrentIP == null) {
+            LVIPList.setAdapter(APIPList);
+            return;
+        }
+
+        csEnd = CSCurrentIP.lastIndexOf('.');
+        if (csEnd == -1) {
+            LVIPList.setAdapter(APIPList);
+            return;
+        }
+        IPTmp = new String(CSCurrentIP.substring(0, csEnd));
+
+        BTScanCount = 0;
+        BTScan.setEnabled(false);
+
+        for (count = 114; count < 256; count++) {
+            //Log.d(TAG, String.format(IPTmp + ".%d", count));
+            ScanThread(String.format(IPTmp + ".%d", count));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/ctrl/music/ipscan/NetTool.java
+++ app/src/main/java/com/ctrl/music/ipscan/NetTool.java
@@ -1,17 +1,25 @@
 package com.ctrl.music.ipscan;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.apache.http.conn.util.InetAddressUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+
 /**
  * Created by suxiaocheng on 3/5/15.
  */
 public class NetTool {
+
+    private static final String TAG = "NetTool";
 
     public NetTool() {
         super();
@@ -44,5 +52,32 @@ public class NetTool {
 
         //System.out.println("本机IP:" + ipaddress);
         return ipaddress;
+    }
+
+    public boolean pingCmdExec(String str) {
+        boolean status = false;
+        Process p;
+        try {
+            //ping -c 3 -w 100  中  ，-c 是指ping的次数 3是指ping 3次 ，-w 100  以秒为单位指定超时间隔，是指超时时间为100秒
+            p = Runtime.getRuntime().exec("ping -c 1 -w 20 " + str);
+            status = (p.waitFor() == 0) ? true : false;
+            InputStream input = p.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+            while ((line = in.readLine()) != null) {
+                buffer.append(line);
+            }
+            Log.d(TAG, String.valueOf(buffer));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return status;
+    }
+
+    public boolean ping(String str) {
+        return pingCmdExec(str);
     }
 }
